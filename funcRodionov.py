8346b724ccacfc2d547cc10768a646ec6cdfe2fa
Vadim000009 <tuukvadim@live.com>
2020-10-17 02:05:35 +0300

    Черновой вариант рабочий Рекурсионный в процессе доработки Стоит пойти спать

--- funcRodionov.py
+++ funcRodionov.py
@@ -5,14 +5,31 @@ def getArrWords(x, y):
         resultStringFst[i] = ord(x[i])
     for j in range(0, len(y)):
         resultStringSec[j] = ord(y[j])
-    for fst in range(0, 26):
-        tempA = resultStringFst[fst]
-        for sec in range(0, 26):
-            tempB = resultStringFst[sec]
-            for thr in range(0, 26):
-                resultStringFst[thr] += 1
-                tempC = resultStringFst[thr]
-                print(chr(tempA) + chr(tempB) + chr(tempC))
-            resultStringFst[sec] += 1
-        resultStringFst[fst] += 1
-    return 0
+    countWords(resultStringFst, 0, resultStringSec, lenOffSecString)
+    # for i in range(0, 26):
+    #     for j in range(0, 26):
+    #         while resultStringFst[2] != 123:
+    #             print(chr(resultStringFst[0]) + chr(resultStringFst[1]) + chr(resultStringFst[2]))
+    #             if resultStringFst[0] == resultStringSec[0] and resultStringFst[1] == resultStringSec[1] and resultStringFst[2] == resultStringSec[2]:
+    #                 return False
+    #             elif resultStringFst[2] == 123:
+    #                 resultStringFst[2] = 97
+    #                 break
+    #             resultStringFst[2] += 1
+    #         resultStringFst[1] += 1
+    #     resultStringFst[0] += 1
+
+def countWords(arrX, pos, finalArr, length) -> bool:
+    while arrX[pos] != 123:
+        if pos != length - 1:
+            pos += 1
+            countWords(arrX, pos, finalArr, length)
+        elif pos == length - 1:
+            for ij in range(0, pos + 1):
+                print(chr(arrX[ij]), end='')
+            print()
+        if (arrX[length - 1] == finalArr[pos]) and pos <= 122:
+            arrX[pos] = 97
+            return False
+        arrX[pos] += 1
+    return True
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from funcRodionov import getArrWords
+from funcRodionov import countWords
 
 if __name__ == "__main__":
     print("Fork")
-    a, b = input(), input()
+    # a, b = input(), input()
+    a, b = "aab", "aba"
     getArrWords(a, b)
