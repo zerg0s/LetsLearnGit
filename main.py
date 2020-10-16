c97a544cd8c6a7dc989281525b9430e4e32631b4
Hsiflix <kirichenko.mai.3@gmail.com>
2020-10-16 15:34:04 +0300

    Task #891338

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import abcExample
+import AlphabetV2
 
 if __name__ == "__main__":
     print("hello world")
     start = input("Enter a start string: ")  # aaa
     finish = input("Enter where to finish: ")  # aax
     abcExample.abc(start, finish)
+    AlphabetV2.alphabetV2(start, finish)
