02c0ae6024583a1d36f81cdb492b5e1122ba810a
Enterprais <34607218+Enterprais@users.noreply.github.com>
2020-10-17 18:19:01 +0300

    add alphabet function
    Taske #891339 resolved

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import abcExample
+import alphabet
 
 if __name__ == "__main__":
     print("hello world")
     start = input("Enter a start string: ")  # aaa
     finish = input("Enter where to finish: ")  # aax
-    abcExample.abc(start, finish)
-    
+    alphabet.abc(start, finish)
