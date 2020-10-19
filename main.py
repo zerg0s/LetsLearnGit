f0b814b489f19742cb64e6637127db08afeb8323
nkovrchk <nkovrchk@gmail.com>
2020-10-19 22:28:51 +0300

    #891345: added alphabet function call

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import abcExample
+import Alphabet_Overchuk
 
 if __name__ == "__main__":
     print("hello world")
     start = input("Enter a start string: ")  # aaa
     finish = input("Enter where to finish: ")  # aax
     abcExample.abc(start, finish)
+    Alphabet_Overchuk.Alphabet(start, finish)
     
