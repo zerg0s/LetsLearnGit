60a88cc00807faf82a118f5954ef836b9fe2a5a2
Smolnaya <57458068+Smolnaya@users.noreply.github.com>
2020-10-20 20:36:27 +0300

    add file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import abcExample
+import abcBySmolnaya
 
 if __name__ == "__main__":
     print("hello world")
     start = input("Enter a start string: ")  # aaa
     finish = input("Enter where to finish: ")  # aax
     abcExample.abc(start, finish)
+    abcBySmolnaya.abc(start, finish)
