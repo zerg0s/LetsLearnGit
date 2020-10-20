871be4992f1afc7cd021e34040b737adcad0b373
igame9 <62247089+igame9@users.noreply.github.com>
2020-10-20 15:11:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import abcStartFinish
+import abcExample
+import Alphabet_Overchuk
+import alphabet
 
 if __name__ == '__main__':
     start = str(input())
     finish = str(input())
     try:
-        print(start)
-        abcStartFinish.abcv4(start, finish)
-        print(finish)
+    abcStartFinish.abcv4(start, finish)
+    abcExample.abc(start, finish)
+    Alphabet_Overchuk.Alphabet(start, finish)
+    alphabet.alphabetV2(start, finish)
     except KeyError:
         print("Для перебора вводятся только буквы")
+        
