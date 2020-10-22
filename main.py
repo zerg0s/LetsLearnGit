c7d4f4e47f237c691fd8cd0b21b4aad725233094
Vadim000009 <tuukvadim@live.com>
2020-10-22 23:43:20 +0300

    correcting!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from funcRodionov import getArrWords
 
 if __name__ == "__main__":
     print("Fork")
+    # ВНИМАНИЕ, ПРОВЕРКИ ДОЛЖЕН НАПИСАТЬ ДРУГОЙ ПОГРАММИСТ И СООТВЕТСТВЕННО СВЕРЯТЬ ДАННЫЕ
+    # НА КОРРЕКТНОСТЬ ЗАДАНЫХ УСЛОВИЙ В СООТВЕТСТВИИ С ЗАДАЧЕЙ
     # a, b = input(), input()
     a, b = "aab", "abb"
     getArrWords(a, b)
