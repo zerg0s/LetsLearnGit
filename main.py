f393bb193c44b0ed7bfab61f590e110355ee5c4c
igame9 <62247089+igame9@users.noreply.github.com>
2020-10-23 12:44:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import alphabet
 import abcExample
+import abcBySmolnaya
 import Alphabet_Overchuk
 import alphabet
 import alphabetClass
@@ -8,20 +10,20 @@ if __name__ == "__main__":
     print("hello world")
     start = input("Enter a start string: ")  # aaa
     finish = input("Enter where to finish: ")  # aax
-    try:
-        abcStartFinish.abcv4(start, finish)
-    except KeyError:
-        print("Ошибка ввода")
-        exit(1)
+    abcExample.abc(start, finish)
+    abcBySmolnaya.abc(start, finish)
+    alphabet.abc(start, finish)
     abcExample.abc(start, finish)
     Alphabet_Overchuk.Alphabet(start, finish)
     alphabet.alphabetV2(start, finish)
     try:
         alphabetClass.Alphabet('abcdefghijklmnopqrstuvwxyz').print_range(start, finish)
+        abcStartFinish.abcv4(start, finish)
     except ValueError as errors:
         print(errors)
         exit(1)
     except KeyError:
         print('Input string have unknown symbol')
         exit(2)
+
         
