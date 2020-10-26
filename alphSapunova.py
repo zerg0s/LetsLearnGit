ae45b70571cd1bc014042c2c12a1df8df6c1ade8
Jene <sapunova12-7@mail.ru>
2020-10-26 18:35:43 +0300

    Fix by Sapunova

--- alphSapunova.py
+++ alphSapunova.py
@@ -1,49 +1,49 @@
 # Алфавит
 # Сапунова Е.С. М3О-109М-20
 
-def firstMoreLast(str1, str2):
-    if len(str1) == 0:
+def firstMoreLast(firstStr, lastStr):
+    if len(firstStr) == 0:
         return True
-    elif ord(str1[0]) <= ord(str2[0]):
-        return firstMoreLast(str1[1:], str2[1:])
+    elif ord(firstStr[0]) <= ord(lastStr[0]):
+        return firstMoreLast(firstStr[1:], lastStr[1:])
     else:
         return False
 
-def iterateLetByLet(s1, s2):
-    if s1 == s2:
-        print(''.join(list(map(chr, s1))))
+def iterateLetByLet(startString, endString):
+    if startString == endString:
+        print(''.join(list(map(chr, startString))))
         return 1
 
-    elif s1[len(s1)-1] != 122:
-        while s1[len(s1)-1] != 122:
-            print(''.join(list(map(chr, s1))))
-            if s1 != s2:
-                s1[len(s1) - 1] += 1
+    elif startString[len(startString)-1] != ord('z'):
+        while startString[len(startString)-1] != ord('z'):
+            print(''.join(list(map(chr, startString))))
+            if sstartString != endString:
+                sstartString[len(startString) - 1] += 1
             else:
                 return 1
-        iterateLetByLet(s1, s2)
+        iterateLetByLet(startString, endString)
 
-    elif s1.index(122) != -1:
+    elif startString.index(ord('z')) != -1:
         try:
-            print(''.join(list(map(chr, s1))))
-            idZ = s1.index(122)
-            s1[idZ-1] += 1
-            i = idZ
-            while i < len(s1):
-                s1[i] = 97
-                i+=1
-            iterateLetByLet(s1, s2)
+            print(''.join(list(map(chr, startString))))
+            identZ = startString.index(ord('z'))
+            startString[identZ-1] += 1
+            i = identZ
+            while i < len(startString):
+                startString[i] = ord('a')
+                i += 1
+            iterateLetByLet(startString, endString)
         except ValueError:
-            print(''.join(list(map(chr, s1))))
-            iterateLetByLet(s1, s2)
+            print(''.join(list(map(chr, startString))))
+            iterateLetByLet(startString, endString)
 
-def alphIterate(string1, string2):
-    if len(string1) == len(string2):
-        if string1.isalpha() and string2.isalpha():
-            if firstMoreLast(string1, string2):
-                str1List = list(map(ord, string1))
-                str2List = list(map(ord, string2))
-                iterateLetByLet(str1List, str2List)
+def alphIterate(firstString, lastString):
+    if len(firstString) == len(lastString):
+        if firstString.isalpha() and lastString.isalpha():
+            if firstMoreLast(firstString, lastString):
+                firstStrList = list(map(ord, firstString))
+                lastStrList = list(map(ord, lastString))
+                iterateLetByLet(firstStrList, lastStrList)
             else:
                 print("Необходимо ввести строки: первая -", \
                 "начало перебора, вторая - конец.", sep='\t', end='\n')
