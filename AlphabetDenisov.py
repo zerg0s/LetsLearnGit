98a33fc0db27d406e99479c4ab55c33af9b14b04
MichaelDenisov <miksel1608@gmail.com>
2020-10-26 17:30:41 +0300

    Alphabet from Michael Denisov (fixed)

--- AlphabetDenisov.py
+++ AlphabetDenisov.py
@@ -1,30 +1,30 @@
-def abcdef(StartString, EndString):
-    StartStringList = []
-    EndStringList = []
+def alphabet(startString, endString):
+    startStringList = []
+    endStringList = []
 
-    for i in range(len(StartString)):
-        StartStringList.append(ord(StartString[i]))
-        EndStringList.append(ord(EndString[i]))
+    for i in range(len(startString)):
+        startStringList.append(ord(startString[i]))
+        endStringList.append(ord(endString[i]))
 
-    while StartStringList != EndStringList:
-        while StartStringList[-1] <= 122:
-            MidString = ''
-            ZCounter = 0
-            for i in range(len(StartString)):
-                MidString += chr(StartStringList[i])
-            print(MidString)
-            if StartStringList == EndStringList:
+    while startStringList != endStringList:
+        while startStringList[-1] <= ord('z'):
+            midString = ''
+            zCounter = 0
+            for i in range(len(startString)):
+                midString += chr(startStringList[i])
+            print(midString)
+            if startStringList == endStringList:
                 break
-            if StartStringList[-1] == 122:
-                for i in reversed(range(len(StartString))):
-                    if StartStringList[i] == 122:
-                        ZCounter += 1
-                        StartStringList.pop(i)
+            if startStringList[-1] == ord('z'):
+                for i in reversed(range(len(startString))):
+                    if startStringList[i] == ord('z'):
+                        zCounter += 1
+                        startStringList.pop(i)
                     else:
                         break
-                StartStringList[-1] += 1
-                for i in range(ZCounter):
-                    StartStringList.append(97)
-                if StartStringList[-1] == 97:
-                    StartStringList[-1] -= 1
-            StartStringList[-1] += 1
+                startStringList[-1] += 1
+                for i in range(zCounter):
+                    startStringList.append(ord('a'))
+                if startStringList[-1] == ord('a'):
+                    startStringList[-1] -= 1
+            startStringList[-1] += 1
