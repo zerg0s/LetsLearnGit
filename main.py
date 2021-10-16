75dd7ac79ad8bedf34698646982a3ac242046174
zerg0s <pul_forever@mail.ru>
2021-10-16 07:57:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,7 @@
-import alphabet
-import abcExample
-import abcKlokov
-import abcBySmolnaya
-import Alphabet_Overchuk
-import alphabet
-import alphabetClass
-import abcStartFinish
-import abcDenisov
-import boreikoAlphabet
-import alphSapunova
-import abcKremeshnaya
-import alphavitByKuznetsov
-import Alphabet_Morozov
-import alphabet_stasyuk
+#import your file
 
 if __name__ == "__main__":
     print("hello world")
     start = input("Enter a start string: ")  # aaa
     finish = input("Enter where to finish: ")  # aax
-    try:
-        alphavitByKuznetsov.abc(start, finish)
-        abcExample.abc(start, finish)
-        abcKlokov.printIntermediateRows(start, finish)
-        abcBySmolnaya.abc(start, finish)
-        alphabet.abc(start, finish)
-        abcExample.abc(start, finish)
-        Alphabet_Overchuk.Alphabet(start, finish)
-        alphabet.alphabetV2(start, finish)
-        abcDenisov.alphabet(start, finish)
-        boreikoAlphabet.alphabetIterating(start, finish)
-        alphSapunova.alphIterate(start, finish)
-        abcKremeshnaya.alphabeticalProcessing(start, finish)
-        Alphabet_Morozov.printSortedStrings(start, finish)
-        alphabetClass.Alphabet('abcdefghijklmnopqrstuvwxyz').print_range(start, finish)
-        abcStartFinish.abcv4(start, finish)
-        alphabet_stasyuk.alphabet(start, finish)
-    except ValueError as errors:
-        print(errors)
-        exit(1)
-    except KeyError:
-        print('Input string have unknown symbol')
-        exit(2)
+    #add your calls here
