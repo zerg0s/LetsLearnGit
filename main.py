ee6c222199dbd736f5ca428ad053a3979abc60f0
YMikhailov99 <87078688+YMikhailov99@users.noreply.github.com>
2021-10-25 14:35:01 +0300

    rename function (#29)

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     print("hello world")
     start = input("Enter a start string: ")  # aaa
     finish = input("Enter where to finish: ")  # aax
-    Alphabet.Alphabet(start, finish)
+    Alphabet.printAlphabet(start, finish)
     printIterationOverStringsAlphabetically(start, finish)
