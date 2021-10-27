20fd453c9d6739ce4952c7ecbc06f84a18fd5bcd
Ilya Belikov <57234143+inSide15@users.noreply.github.com>
2021-10-27 09:05:21 +0300

    Added a new way to solve the alphabet problem (#32)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import Alphabet
 from alphabet_task import printIterationOverStringsAlphabetically
+import AlphabetGit
 
 if __name__ == "__main__":
     print("hello world")
@@ -7,3 +8,7 @@ if __name__ == "__main__":
     finish = input("Enter where to finish: ")  # aax
     Alphabet.printAlphabet(start, finish)
     printIterationOverStringsAlphabetically(start, finish)
+
+    startStr = input("Enter a start string: ")
+    finishStr = input("Enter where to finish: ")
+    AlphabetGit.printAlphabet(startStr, finishStr)
