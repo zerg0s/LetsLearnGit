44db9792869b4529790d7a7e422ad3cd905a388c
Ilya Danilov <Qiwiforgame@gmail.com>
2021-10-27 21:32:43 +0300

    Added a new solution by Danilov Ilya

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import Alphabet
 from alphabet_task import printIterationOverStringsAlphabetically
 from ae_belousov_alphabet import print_char_combinations
 import AlphabetGit
+import abcByDanilov
 
 if __name__ == "__main__":
     print("Alphabet task has started :)")
     start = input("Enter a start string: ")  # aaa
     finish = input("Enter where to finish: ")  # aba
-    
+
     print("First solution")
     printIterationOverStringsAlphabetically(start, finish)
-    
+
     print("Second Solution")
-    AlphabetGit.printAlphabet(startStr, finishStr)
+    AlphabetGit.printAlphabet(start, finish)
 
     print("3rd Solution")
     print_char_combinations(start, finish)
-    
+
     print("4th Solution")
     Alphabet.printAlphabet(start, finish)
+
+    print("5th Solution")
+    abcByDanilov.alp_start(start, finish)
