e7d78f30a73c31e347634511bbd83411d2873e85
zerg0s <pul_forever@mail.ru>
2021-10-27 12:01:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from alphabet_task import printIterationOverStringsAlphabetically
 import AlphabetGit
 
 if __name__ == "__main__":
-    print("hello world")
+    print("Alphabet task has started :)")
     start = input("Enter a start string: ")  # aaa
     finish = input("Enter where to finish: ")  # aax
     Alphabet.printAlphabet(start, finish)
+    print("First solution")
     printIterationOverStringsAlphabetically(start, finish)
-
-    startStr = input("Enter a start string: ")
-    finishStr = input("Enter where to finish: ")
+    print("Second Solution")
     AlphabetGit.printAlphabet(startStr, finishStr)
