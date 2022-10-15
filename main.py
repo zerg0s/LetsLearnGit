bf134b3b1c97043e9e37b66d083ddf30ea89eec1
zerg0s <pul_forever@mail.ru>
2022-10-15 11:37:24 +0300

    2023 ready

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import Alphabet
-from alphabet_task import printIterationOverStringsAlphabetically
-from ae_belousov_alphabet import print_char_combinations
-import AlphabetGit
-import abcByDanilov
-import alphabet_Bochkareva
-import TaskChizh
-import alphabetByDiana
+# import <your_solution>
 
 
 if __name__ == "__main__":
@@ -14,25 +7,6 @@ if __name__ == "__main__":
     finish = input("Enter where to finish: ")  # aba
 
     print("First solution")
-    printIterationOverStringsAlphabetically(start, finish)
-
-    print("Second Solution")
-    AlphabetGit.printAlphabet(start, finish)
-
-    print("3rd Solution")
-    print_char_combinations(start, finish)
-
-    print("4th Solution")
-    Alphabet.printAlphabet(start, finish)
-
-    print("5th Solution")
-    abcByDanilov.alp_start(start, finish)
+    #printIterationOverStringsAlphabetically(start, finish)
+    #...
     
-    print("6th Solution")
-    alphabet_Bochkareva.abc(start, finish)
-
-    print("7th Solution")
-    TaskChizh.IterateStrings(start, finish)
-    
-    print("8th Solution")
-    alphabetByDiana.checkAssepted(start, finish)
