a49bd6dd9b8797e2973ab9e496e17c05e5fd2389
gachiCULTIST <301nikotrin301@gmail.com>
2022-10-20 01:09:36 +0300

    add gachi solution for string span printing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Alfavit import printIterationOverStringsAlphabetically
+from gachiResolution import print_string_interval
 
 if __name__ == "__main__":
     print("Alphabet task has started :)")
@@ -7,3 +8,9 @@ if __name__ == "__main__":
 
     print("First solution")
     printIterationOverStringsAlphabetically(start, finish)
+
+    print("Gachi solution")
+    try:
+        print_string_interval(start, finish)
+    except RuntimeError as ex:
+        print(ex)
