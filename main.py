2d515a47af842b26bf1613e51854be09fe71023b
Morich <MorichBlackWing@gmail.com>
2022-10-20 15:33:25 +0300

    Task alphabet. Adds another one solution for the task (output a full enumeration of lines from first to second in alphabetical order).

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Alfavit import printIterationOverStringsAlphabetically
 from gachiResolution import print_string_interval
+from alphabet import print_alphabet_values
 
 if __name__ == "__main__":
     print("Alphabet task has started :)")
@@ -14,3 +15,6 @@ if __name__ == "__main__":
         print_string_interval(start, finish)
     except ValueError as ex:
         print(ex)
+
+    print("Third solution")
+    print_alphabet_values(start, finish)
