01024cdf664bafdd02c192298b1a6088013cc63c
Максим Радаев <m.radaev.m.r.123@bk.ru>
2022-10-21 22:12:52 +0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Alfavit import printIterationOverStringsAlphabetically
 from gachiResolution import print_string_interval
 from alphabet import print_alphabet_values
+from alfavit_e import gen_generation
 
 if __name__ == "__main__":
     print("Alphabet task has started :)")
@@ -18,3 +19,6 @@ if __name__ == "__main__":
 
     print("Third solution")
     print_alphabet_values(start, finish)
+
+    print("Maxim solution")
+    gen_generation(start, finish)
