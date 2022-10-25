e6b097dca8f6efdd000ad9d09465e4f885ec84dc
Xavierou <i.dont.know.what.to.write122@gmail.com>
2022-10-25 12:17:59 +0300

    Added solution by Denis Arkhipov. Resolving conflict.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from gachiResolution import print_string_interval
 from alfavit_e import gen_generation
 from alphabet import print_alphabet_values
 from alphabetByRoman import dummyAlphabet
+from alphabet_pashtet import printAlphabetByPashtet
+from XavierAlphabet import ascii_alphabet_solution
 
 if __name__ == "__main__":
     print("Alphabet task has started :)")
@@ -30,3 +32,11 @@ if __name__ == "__main__":
     
     print("Roman's solution")
     dummyAlphabet(start, finish)
+
+    print("Pashtet's solution")
+    printAlphabetByPashtet(start, finish)
+
+    try:
+        ascii_alphabet_solution(start, finish)
+    except ValueError as ex:
+        print(ex)
