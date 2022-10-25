28f89c645f59295b0cc9ffb5e874fbf9759ec4be
BETEPOK3 <63142099+BETEPOK3@users.noreply.github.com>
2022-10-25 08:49:44 +0300

    Updated by Pahtet (#58)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from gachiResolution import print_string_interval
 from alfavit_e import gen_generation
 from alphabet import print_alphabet_values
 from alphabetByRoman import dummyAlphabet
+from alphabet_pashtet import printAlphabetByPashtet
 
 if __name__ == "__main__":
     print("Alphabet task has started :)")
@@ -30,3 +31,6 @@ if __name__ == "__main__":
     
     print("Roman's solution")
     dummyAlphabet(start, finish)
+
+    print("Pashtet's solution")
+    printAlphabetByPashtet(start, finish)
