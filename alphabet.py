5c1b6faeea9f5d91ebf60a5fba7919bcbc50af18
bondarenko.ilya <bondarenkoi07@yandex.ru>
2022-10-29 16:47:59 +0300

    [Task #951895 ] add yet another string ordering script

---
+++ alphabet.py
@@ -0,0 +1,41 @@
+TWENTY_FIVE = ord('z')
+ZERO = ord('a')
+BASE = TWENTY_FIVE - ZERO + 1
+
+
+class AlphabetNumber:
+    @staticmethod
+    def ToAlphabet(number: int, power: int) -> str:
+        alph_number = ""
+        while number > 0:
+            alph_number = chr(ZERO + number % BASE) + alph_number
+            number //= BASE
+
+        if len(alph_number) < power:
+            for _ in range(0, power - len(alph_number)):
+                alph_number = chr(ZERO) + alph_number
+
+        return alph_number
+
+    @staticmethod
+    def ToDec(alph_number: str) -> int:
+        order = 0
+        number = 0
+        for rune in reversed(alph_number):
+            number += (ord(rune) - ZERO) * BASE ** order
+            order += 1
+
+        return number
+
+
+def PrintAlphabetNumbers(start: str, finish: str):
+    start_dec = AlphabetNumber.ToDec(start)
+    print(f"start {start} dec {start_dec}")
+    finish_dec = AlphabetNumber.ToDec(finish)
+    print(f"finish {finish} dec {finish_dec}")
+
+    for i in range(start_dec, finish_dec + 1):
+        print(AlphabetNumber.ToAlphabet(i, len(start)))
+
+
+PrintAlphabetNumbers("bcd", "bza")
