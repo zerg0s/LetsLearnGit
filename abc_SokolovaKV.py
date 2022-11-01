c5db468936efdd07abf5953112ddb4220346301b
Aleksey <117095683+KseniiiaSokolova@users.noreply.github.com>
2022-11-01 23:16:18 +0300

    Update3 SokolovaKV

--- abc_SokolovaKV.py
+++ abc_SokolovaKV.py
@@ -10,27 +10,24 @@ def get_next(str1):
     retval= get_next(retval)
     retval+= 'a' * (i-1)
     return retval
-def containNumbers(value, l):
-    for i in l:
-        if i in value:
-            return 1
-    return 0
+def containNumbers(value):
+    if value.isalpha():
+            return 0
+    return 1
 def generalFunc(str1,str2):
-    list_ = ["0","1","2","3","4","5","6","7","8","9"]
-    try:
-##        str1 = str(input())
-##        str2 = str(input())
-        if containNumbers(str1, list_) == 1 or containNumbers(str2, list_) == 1:
-            print('введенные значения содержат цифры')
-        elif str1 == str2:
+    if containNumbers(str1) == 1 or containNumbers(str2) == 1:
+            print('введенные значения содержат цифры/или отсутствуют значения')
+    elif str1 == str2:
             print('Значения строк равны!')
-        else:
+    else:
+        try:
             if len(str1) == len(str2):
                 tmp= get_next(str1)
                 while tmp!= str2:
-                    print(tmp)
-                    tmp= get_next(tmp)
+                        print(tmp)
+                        tmp= get_next(tmp)
             else:
-                print('Error: Разные длины строк ')
-    except IndexError:
-        print('Error: Индекс за пределами ')
+                    print('Error: Разные длины строк ')
+        except IndexError:
+            print('Error: Индекс за пределами ')
+
